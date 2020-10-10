b46f218fd15fa713d31f41bfbb91ef9618fc3c34
cyfreeman <cywxm0118@163.com>
2020-10-10 17:58:01 +0800

    initial commit

---
+++ cystudy.springcloud/cloud-provider-payment8001/src/main/java/cy/study/springcloud/entities/payment.java
@@ -0,0 +1,15 @@
+package cy.study.springcloud.entities;
+
+
+import lombok.Data;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class payment {
+    private Long id;
+    
+}
---
+++ springcloud/payment8001/src/test/java/cy/study/springcloud/SpringcloudApplicationTests.java
@@ -0,0 +1,13 @@
+package cy.study.springcloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringcloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
